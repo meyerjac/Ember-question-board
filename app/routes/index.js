3082f7ac9111d6a125a0460261df477f4f55716f
Jackson Meyer <Guest@epicodus-43.local>
2016-10-28 16:52:48 -0700

    just starting the multiple model relationships

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('question');
+    questions: this.store.findAll('question'),
+    answers: this.store.findAll('answer')
   },
 
   actions: {
