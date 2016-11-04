627cbace86e7b0df29a2ffbf4ae0dc5e71b2a152
Jackson Meyer <Guest@epicodus-99.local>
2016-11-04 09:16:27 -0700

    add a helper to indicate which questions have had the most activity of answers

---
+++ app/helpers/most-answers.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export function mostAnswers(params) {
+  var numberOfAnswers = params[0].get('answers').get('length');
+  console.log(numberOfAnswers);
+  // console.log(numberOfAnswers.__ember_meta__._cache.promise);
+  if (numberOfAnswers >= 4) {
+    return Ember.String.htmlSafe('<span class="lightbulb"> !!!</span>')
+  } else if (numberOfAnswers >= 1) {
+    return Ember.String.htmlSafe('<span class="lightbulb"> ! </span>')
+  } else {
+    return Ember.String.htmlSafe('<span class="lightbulb"> NO ANSWERS YET</span>')
+  }
+}
+
+export default Ember.Helper.helper(mostAnswers);
